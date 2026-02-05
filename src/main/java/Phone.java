5776296b0d150f23a4c1fb33a3eb117fa85d2920
Christian P <113365748+cperona@users.noreply.github.com>
2026-02-05 14:20:17 +0100

    feat: add Phone class

---
+++ src/main/java/Phone.java
@@ -0,0 +1,32 @@
+public class Phone {
+    private String brand;
+    private String model;
+
+    public Phone(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void call(int phoneNum) {
+        System.out.println("Calling Phone "+phoneNum);
+    }
+
+
+
+}
