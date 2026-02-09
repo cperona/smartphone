b953e8eb7640166f8b205c28edd360e6508745aa
Christian P <113365748+cperona@users.noreply.github.com>
2026-02-09 12:35:19 +0100

    feat: add Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Smartphone smartphone = new Smartphone("brand24", "model24");
+        smartphone.shot();
+        smartphone.triggerAlarm();
+    }
+}
